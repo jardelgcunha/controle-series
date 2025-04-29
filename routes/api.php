108818e34ad0843039d7560e9def8e01691b7946
Jardel Cunha <jardelgcunha.ti@gmail.com>
2025-04-29 15:26:43 -0300

    add endpoint to watch episodes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\SeasonsController;
 use App\Http\Controllers\Api\SeriesController;
+use App\Models\Episode;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,10 @@ Route::apiResource('/series', SeriesController::class);
 Route::get('/series/{series}/seasons', [SeasonsController::class, 'show']);
 
 Route::get('/series/{series}/episodes', [SeriesController::class, 'getEpisodes']);
+
+Route::patch('/episodes/{episode}', function (Episode $episode, Request $request) {
+    $episode->watched = $request->watched;
+    $episode->save();
+    
+    return $episode;
+});
